cc4207bf4f65854778363e3078b24d8b1833f7ab
itsubaki <1759459+itsubaki@users.noreply.github.com>
2021-10-02 21:52:02 +0900

    Add tensor product for matrix

--- src/quantum.rs
+++ src/quantum.rs
@@ -4,6 +4,36 @@ pub type Qubit = Vec<Complex<f64>>;
 
 pub type Gate = Vec<Vec<Complex<f64>>>;
 
+pub fn tensor_product(m: Gate, n: Gate) -> Gate {
+    let p = m.len();
+    let q = m[0].len();
+    let a = n.len();
+    let b = n[0].len();
+
+    let mut out: Gate = vec![];
+    for i in 0..p {
+        for k in 0..a {
+            let mut v: Vec<Complex<f64>> = vec![];
+            for j in 0..q {
+                for l in 0..b {
+                    v.push(m[i][j] * n[k][l]);
+                }
+            }
+
+            out.push(v);
+        }
+    }
+
+    return out;
+}
+
+fn id() -> Gate {
+    return vec![
+        vec![Complex { re: 1.0, im: 0.0 }, Complex { re: 0.0, im: 0.0 }],
+        vec![Complex { re: 0.0, im: 0.0 }, Complex { re: 1.0, im: 0.0 }],
+    ];
+}
+
 fn x() -> Gate {
     return vec![
         vec![Complex { re: 0.0, im: 0.0 }, Complex { re: 1.0, im: 0.0 }],
@@ -66,20 +96,38 @@ impl Q {
     }
 
     pub fn x(&mut self, qb: &[u32]) {
-        self.apply(x(), qb);
+        let mut g = id();
+        if qb[0] == 0 {
+            g = x();
+        }
+
+        for i in 0..self.number_of_bit() {
+            for j in 0..qb.len() {
+                if i == qb[j] {
+                    g = tensor_product(g, x());
+                    continue;
+                }
+                g = tensor_product(g, id());
+            }
+        }
+
+        self.apply(g);
     }
 
     pub fn h(&mut self, qb: &[u32]) {
-        self.apply(h(), qb);
+        self.apply(h());
     }
 
     pub fn cmodexp2(&mut self, a: u32, n: u32, r0: &[u32], r1: &[u32]) {}
 
     pub fn iqft(&mut self, qb: &[u32]) {}
 
-    pub fn apply(&mut self, g: Gate, qb: &[u32]) {}
+    pub fn apply(&mut self, g: Gate) {
+        let xx = tensor_product(x(), x());
+        println!("xx: {:?}", xx);
+    }
 
-    pub fn tensor_product(&mut self, qb: Qubit) {
+    fn tensor_product(&mut self, qb: Qubit) {
         let mut v = vec![];
         for i in 0..self.qb.len() {
             for j in 0..qb.len() {
